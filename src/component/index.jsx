d4b916c5cbbc8574b33e0d272a1028765401ac71
Mohammad Ashif <Mohammad Ashif>
2024-02-23 16:07:59 +0600

    multiselection added

--- src/component/index.jsx
+++ src/component/index.jsx
@@ -4,28 +4,57 @@ import "./style.css";
 
 const Accordion = () => {
     const [selected, setSelected] = useState(null);
+    const [enable, setEnable] = useState(false);
+    const [multiSelected, setMultiSelected] = useState([]);
 
     const handleSingleSelectedData = (id) => {
         setSelected(id === selected ? null : id);
     };
 
+    const handleMultiSelection = (id) => {
+        let cpyMultiple = [...multiSelected]; 
+        const findIndexOfCurrentId = cpyMultiple.indexOf(id);
+        
+        if(findIndexOfCurrentId === -1) cpyMultiple.push(id);
+        else cpyMultiple.splice(findIndexOfCurrentId, 1);
+        setMultiSelected(cpyMultiple);
+    };
+
+    console.log(selected, multiSelected);
+
     return (
         <div className="wrapper">
+            {enable ? (
+                <button onClick={() => setEnable(!enable)}>
+                    Disable Multi Selection
+                </button>
+            ) : (
+                <button onClick={() => setEnable(!enable)}>
+                    Enable Multi Selection
+                </button>
+            )}
+
             <div className="accordion">
-                {accordionData.map((item) => (
-                    <div className="item" key={item.id}>
-                        <div
-                            onClick={() => handleSingleSelectedData(item.id)}
-                            className="title"
-                        >
-                            <p>{item.question}</p>
-                            {selected ? <span>-</span> : <span>+</span>}
+                {accordionData && accordionData.length > 0 ? (
+                    accordionData.map((item) => (
+                        <div className="item" key={item.id}>
+                            <div
+                                onClick={enable ? ()=> handleMultiSelection(item.id):() =>
+                                    handleSingleSelectedData(item.id)
+                                }
+                                className="title"
+                            >
+                                <p>{item.question}</p>
+                                {selected ? <span>-</span> : <span>+</span>}
+                            </div>
+                            {selected === item.id || multiSelected.indexOf(item.id) !== -1 ? (
+                                <p className="content">{item.answer}</p>
+                            ) : null}
                         </div>
-                        {selected === item.id ? (
-                            <p className="content">{item.answer}</p>
-                        ) : null}
-                    </div>
-                ))}
+                    ))
+                ) : (
+                    <p>Data not found</p>
+                )}
             </div>
         </div>
     );
